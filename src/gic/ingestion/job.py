317921e8d270005599ccc6f3c8357416e3b84efc
Shu Heng Khor <shuhengkhor@Shus-MacBook-Pro.local>
2025-11-13 10:12:58 +0800

    Ingestion and pricing reconciliation report

--- src/gic/ingestion/job.py
+++ src/gic/ingestion/job.py
@@ -2,9 +2,13 @@ import sys
 
 from pyspark.sql import SparkSession
 
-from src.ingestion.external_funds import ingest_external_funds, Config as IngestionConfig
+from gic.ingestion.external_funds import (
+    ingest_external_funds,
+    Config as IngestionConfig,
+)
 
 import argparse
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -14,24 +18,28 @@ def parse_args() -> argparse.Namespace:
 
 
 def create_spark_session() -> SparkSession:
-    spark_session = SparkSession \
-        .builder \
-        .master("local[*]") \
-        .config("spark.jars", "jars/sqlite-jdbc-3.51.0.0.jar") \
-        .config("spark.driver.extraClassPath", "jars/sqlite-jdbc-3.51.0.0.jar") \
+    spark_session = (
+        SparkSession.builder.master("local[*]")
+        .config("spark.jars", "jars/sqlite-jdbc-3.51.0.0.jar")
+        .config("spark.driver.extraClassPath", "jars/sqlite-jdbc-3.51.0.0.jar")
         .getOrCreate()
+    )
 
     return spark_session
+
 
 if __name__ == "__main__":
     spark_session = None
     try:
         spark_session = create_spark_session()
         config = parse_args()
-        ingest_external_funds(spark_session, IngestionConfig(
-            src_dir=config.src_dir,
-            dest_url=config.dest_url,
-        ))
+        ingest_external_funds(
+            spark_session,
+            IngestionConfig(
+                src_dir=config.src_dir,
+                dest_url=config.dest_url,
+            ),
+        )
 
     except Exception as e:
         print(f"Error while running job: {e}", file=sys.stderr)
